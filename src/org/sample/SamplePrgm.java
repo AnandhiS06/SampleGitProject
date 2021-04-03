ba33b5faf6992ba8118dc28d2d7fc1d7d69c65a9
AnandhiS06 <anandhi.bme06@gmail.com>
2021-04-03 13:42:46 +0530

    sample prgm

---
+++ src/org/sample/SamplePrgm.java
@@ -0,0 +1,10 @@
+package org.sample;
+
+public class SamplePrgm {
+
+	public static void main(String[] args) {
+		System.out.println("Sample Java Program");
+		int a=20;
+		System.out.println(a);
+	}
+}
